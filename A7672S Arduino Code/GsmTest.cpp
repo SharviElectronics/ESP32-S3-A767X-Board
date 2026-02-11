c6a2d6ae5ce01e342a4d110326f3f68294b37034
Rajkumar181 <suryadevkuswaha@gmail.com>
2026-02-11 16:39:25 +0530

    Create GsmTest.cpp

---
+++ A7672S Arduino Code/GsmTest.cpp
@@ -0,0 +1,38 @@
+#define A7672_RX 18  // ESP32 pin connected to A7672 TX
+#define A7672_TX 17  // ESP32 pin connected to A7672 RX
+#define PW_KEY  	10  
+
+void setup() {
+  pinMode(PW_KEY, OUTPUT);
+  Serial.begin(115200);              // Monitor
+  Serial2.begin(115200, SERIAL_8N1, A7672_RX,  A7672_TX);  // EC200U UART
+  digitalWrite(PW_KEY, LOW);
+  delay(3000);
+  Serial.println("Initializing A7672...");
+
+  sendAT("AT", "OK");
+  sendAT("AT+CMGF=1", "OK");  // Set SMS to text mode
+  sendAT("AT+CMGS=\"+9199XXXXXXXX\"", ">");  // Replace with your number
+  Serial2.print("Hello from A7672!\r");   // SMS content
+  Serial2.write(0x1A);  // Ctrl+Z to send
+  Serial.println("SMS Sent!");
+}
+
+void loop() {
+  // Nothing to do in loop
+}
+// Function to send AT command and wait for expected response
+void sendAT(String command, String expectedResponse, unsigned long timeout = 3000) {
+  Serial2.println(command);
+  unsigned long t = millis();
+  String response = "";
+  while (millis() - t < timeout) {
+    while (Serial2.available()) {
+      char c = Serial2.read();
+      response += c;
+    }
+    if (response.indexOf(expectedResponse) != -1) break;
+  }
+  Serial.println("CMD: " + command);
+  Serial.println("RESP: " + response);
+}
